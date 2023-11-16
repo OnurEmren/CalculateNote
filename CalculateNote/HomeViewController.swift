ed34bbe3676ac89180498ed9527d7904188bc407
OnurEmren <developeronur1@gmail.com>
2023-11-16 14:52:24 +0300

    saved data

--- CalculateNote/HomeViewController.swift
+++ CalculateNote/HomeViewController.swift
@@ -11,25 +11,57 @@ class HomeViewController: UIViewController {
     
     var tableView: UITableView!
     var studentList = [Students]()
+    var studentView = StudentsView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadStudents()
         setupTableView()
         createStudentList()
-        
+        setTableView()
+        setupTapGestureRecognizer()
         view.backgroundColor = .systemMint
     }
     
-    func createStudentList() {
-        for i in 1...40 {
-            let student = Students(name: "Öğrenci \(i)")
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.reloadData()
+    }
+    
+    private func setTableView() {
+        studentView.nameTextField.text = ""
+        studentView.gradeTextField1.text = ""
+        studentView.gradeTextField2.text = ""
+        studentView.gradeTextField3.text = ""
+    }
+    
+    private func saveStudents() {
+        do {
+            // Öğrenci listesini Data'ya dönüştür ve UserDefaults'a kaydet
+            let encodedData = try JSONEncoder().encode(studentList)
+            UserDefaults.standard.set(encodedData, forKey: "studentsKey")
+        } catch {
+            print("Verileri kaydederken bir hata oluştu: \(error.localizedDescription)")
+        }
+    }
+    
+    private func loadStudents() {
+        if let savedData = UserDefaults.standard.data(forKey: "studentsKey"),
+           let loadedStudents = try? JSONDecoder().decode([Students].self, from: savedData) {
+            studentList = loadedStudents
+        }
+    }
+    
+    private func createStudentList() {
+        for _ in 1...40 {
+            let student = Students(name: "")
             studentList.append(student)
         }
     }
     
-    
-    func setupTableView() {
+    private func setupTableView() {
         tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
@@ -43,46 +75,47 @@ class HomeViewController: UIViewController {
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
         }
         
-        let footerView = UIView()
         let calculateButton = UIButton(type: .system)
         calculateButton.setTitle("Hesapla", for: .normal)
         calculateButton.tintColor = .white
         calculateButton.addTarget(self, action: #selector(calculateButtonTapped), for: .touchUpInside)
-        footerView.addSubview(calculateButton)
+        view.addSubview(calculateButton)
         
         calculateButton.snp.makeConstraints { (make) in
-            make.center.equalToSuperview()
-        }
-        
-        view.addSubview(footerView)
-        
-        footerView.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(-30)
             make.height.equalTo(40)
             make.width.equalTo(100)
-
         }
     }
     
-    @objc func calculateButtonTapped() {
+    @objc private func calculateButtonTapped() {
         for (index, student) in studentList.enumerated() {
-            // Her öğrencinin notlarını al
             let grade1 = student.grades[0] * 0.5
             let grade2 = student.grades[1] * 0.25
             let grade3 = student.grades[2] * 0.25
-
+            
             // Ortalamayı hesapla
             let average = (grade1 + grade2 + grade3)
-
+            
             // Her öğrencinin resultLabel'ına not ortalamasını yerleştir
             if let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0)) as? StudentsView {
                 cell.updateResultLabel(withAverage: average)
             }
         }
+        saveStudents()
     }
 
     
+    private func setupTapGestureRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc 
+    private func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -94,12 +127,28 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath) as! StudentsView
         let student = studentList[indexPath.row]
         cell.student = student
-        cell.nameLabel.text = "\(student.name)"
+        cell.nameTextField.delegate = cell
+        cell.gradeTextField1.delegate = cell
+        cell.gradeTextField2.delegate = cell
+        cell.gradeTextField3.delegate = cell
+        cell.updateUI(with: student)
+        cell.updateResultLabel(withAverage: calculateAverage(for: student))
         cell.backgroundColor = .systemMint
         return cell
     }
     
+    func calculateAverage(for student: Students) -> Double {
+        let grade1 = student.grades[0] * 0.5
+        let grade2 = student.grades[1] * 0.25
+        let grade3 = student.grades[2] * 0.25
+        return (grade1 + grade2 + grade3)
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
--- CalculateNote/StudentsView.swift
+++ CalculateNote/StudentsView.swift
@@ -9,9 +9,9 @@ import Foundation
 import SnapKit
 import UIKit
 
-class StudentsView: UITableViewCell {
+class StudentsView: UITableViewCell, UITextFieldDelegate {
     
-    var nameLabel: UILabel!
+    var nameTextField: UITextField!
     var resultLabel: UILabel!
     var gradeTextField1: UITextField!
     var gradeTextField2: UITextField!
@@ -26,6 +26,12 @@ class StudentsView: UITableViewCell {
         
         setupUI()
         setupConstraints()
+        updateResultLabel(withAverage: 0.0)
+        
+        nameTextField.delegate = self
+        gradeTextField1.delegate = self
+        gradeTextField2.delegate = self
+        gradeTextField3.delegate = self
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,43 +41,70 @@ class StudentsView: UITableViewCell {
     }
     
     func setupUI() {
-        nameLabel = UILabel()
-        contentView.addSubview(nameLabel)
+        nameTextField = UITextField()
+        nameTextField.borderStyle = .roundedRect
+        nameTextField.placeholder = "Öğrenci Adı"
+        contentView.addSubview(nameTextField)
         
         gradeTextField1 = UITextField()
         gradeTextField1.borderStyle = .roundedRect
+        gradeTextField1.placeholder = "Not1"
+        gradeTextField1.keyboardType = .numberPad
         contentView.addSubview(gradeTextField1)
         
         gradeTextField2 = UITextField()
         gradeTextField2.borderStyle = .roundedRect
+        gradeTextField2.keyboardType = .numberPad
         contentView.addSubview(gradeTextField2)
         
         gradeTextField3 = UITextField()
         gradeTextField3.borderStyle = .roundedRect
+        gradeTextField3.keyboardType = .numberPad
         contentView.addSubview(gradeTextField3)
         
-        gradeTextFields = [gradeTextField1, gradeTextField2, gradeTextField3] // gradeTextFields dizisi oluşturuldu
-
+        gradeTextFields = [gradeTextField1, gradeTextField2, gradeTextField3]
+        
         for (index, gradeTextField) in gradeTextFields.enumerated() {
             gradeTextField.tag = index
             gradeTextField.addTarget(self, action: #selector(gradeTextFieldDidChange(_:)), for: .editingChanged)
         }
         
         resultLabel = UILabel()
-             resultLabel.textAlignment = .center
-             resultLabel.textColor = .black
-             resultLabel.font = UIFont.systemFont(ofSize: 16)
-             contentView.addSubview(resultLabel)
-
-             resultLabel.snp.makeConstraints { (make) in
-                 make.centerX.equalToSuperview()
-                 make.bottom.equalToSuperview().offset(-5)
-             }
+        resultLabel.textAlignment = .center
+        resultLabel.textColor = .white
+        resultLabel.font = .boldSystemFont(ofSize: 18)
+        contentView.addSubview(resultLabel)
+        
+        resultLabel.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-5)
+        }
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let indexPath = getIndexPath(for: textField), let text = textField.text {
+            // Öğrencinin adını ya da notlarını güncelle
+            switch textField {
+            case nameTextField:
+                student?.name = text
+            case gradeTextField1:
+                student?.grades[0] = Double(text) ?? 0
+            case gradeTextField2:
+                student?.grades[1] = Double(text) ?? 0.0
+            case gradeTextField3:
+                student?.grades[2] = Double(text) ?? 0.0
+            default:
+                break
+            }
+        }
     }
     
     func updateResultLabel(withAverage average: Double) {
-            resultLabel.text = "Ortalama: \(average)"
-        }
+        resultLabel.text = "Ortalama: \(String(format: "%.2f", average))"
+        resultLabel.isHidden = false
+    }
+    
     
     @objc func gradeTextFieldDidChange(_ textField: UITextField) {
         print("Grade text field changed: \(textField.text ?? "nil")")
@@ -82,30 +115,54 @@ class StudentsView: UITableViewCell {
     }
     
     func setupConstraints() {
-        nameLabel.snp.makeConstraints { (make) in
+        nameTextField.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(10)
             make.left.equalToSuperview().offset(20)
         }
         
         gradeTextField1.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(40)
+            make.top.equalToSuperview().offset(50)
             make.left.equalToSuperview().offset(20)
-            make.width.equalTo(50)
-            make.height.equalTo(30)
+            make.width.equalTo(60)
+            make.height.equalTo(40)
         }
         
         gradeTextField2.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(40)
+            make.top.equalToSuperview().offset(50)
             make.left.equalToSuperview().offset(90)
-            make.width.equalTo(50)
-            make.height.equalTo(30)
+            make.width.equalTo(60)
+            make.height.equalTo(40)
         }
         
         gradeTextField3.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(40)
+            make.top.equalToSuperview().offset(50)
             make.left.equalToSuperview().offset(160)
-            make.width.equalTo(50)
-            make.height.equalTo(30)
+            make.width.equalTo(60)
+            make.height.equalTo(40)
         }
     }
+    
+    func updateUI(with student: Students) {
+        nameTextField.text = student.name
+        gradeTextField1.text = "\(student.grades[0])"
+        gradeTextField2.text = "\(student.grades[1])"
+        gradeTextField3.text = "\(student.grades[2])"
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+           // TextField içine tıklandığında varsayılan değeri temizle
+           textField.text = ""
+       }
 }
+
+extension UITableViewCell {
+    func getIndexPath(for textField: UITextField) -> IndexPath? {
+        if let tableView = superview as? UITableView {
+            let point = convert(bounds.origin, to: tableView)
+            return tableView.indexPathForRow(at: point)
+        }
+        return nil
+    }
+}
+
